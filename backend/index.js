f06fdd71ea93f5d50da6d1438c335f76bc1f29cb
m-swelam <mid2o0o11@gmail.com>
2021-12-03 21:56:17 +0200

    mongoDB connect commit

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 const express = require("express")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+
 const app = express()
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("connectedto mongodb"))
+  .catch((err) => console.log(err))
 
 app.listen("5000", () => {
   console.log("Backend is Running")
